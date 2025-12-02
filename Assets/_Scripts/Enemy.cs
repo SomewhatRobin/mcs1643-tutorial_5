7d822d48485b833cda487fdefb01352e3513bba7
SomewhatRobin <jrobinso3@ltu.edu>
2025-12-02 12:56:48 -0500

    Vid 2 Done
    Aeiou

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -7,22 +7,62 @@ public class Enemy : MonoBehaviour
 {
     public float HP = 25.0f;
     public GameObject ExplosionPrefab;
-    public Transform Goal;
+    public Transform[] Waypoints;
+    public GameObject EyeObject;
 
     private NavMeshAgent Agent;
+    private int curDest;
+    private bool SpottedPlayer;
+    private GameObject Player;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Agent = GetComponent<NavMeshAgent>();
-        Agent.SetDestination(Goal.position);
+        Agent.SetDestination(Waypoints[0].position);
+        curDest = 0;
+        SpottedPlayer = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Check if I can see the player
+        RaycastHit hitInfo;
+        bool hit = Physics.Raycast(EyeObject.transform.position, transform.forward, out hitInfo);
+
+        if(hit)
+        {
+            if (hitInfo.transform.CompareTag("Player") )
+            {
+                Debug.Log($"Found you.");
+                SpottedPlayer = true;
+                Player = hitInfo.transform.gameObject;
+                Agent.SetDestination(hitInfo.transform.position);
+            }
+
+        }
+
+        if (SpottedPlayer)
+        {
+            Agent.SetDestination(Player.transform.position);
+        }
+
+        if (!SpottedPlayer)
+        {
+            if (Agent.pathStatus == NavMeshPathStatus.PathComplete && Agent.remainingDistance < .1f)
+            {
+                curDest++;
+                if (curDest == Waypoints.Length)
+                {
+                    curDest = 0;
+                }
+                Agent.SetDestination(Waypoints[curDest].position);
+            }
+        }
+
     }
 
     public void TakeDamage(float damage)
